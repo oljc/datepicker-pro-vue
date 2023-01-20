080d6aec6e36888d0d8a918125ed666187fb80cc
李江辰 <SpaceCode@foxmail.com>
2023-01-20 18:47:27 +0800

    build: Update babel.config file to adapt jest

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   presets: [
     [
-      '@babel/env',
+      '@babel/preset-env',
       {
-        modules: false, // 关闭 esm 转化，统一交由 rollup 处理，防止冲突
+        targets: {
+          node: 'current',
+        },
       },
     ],
   ],
